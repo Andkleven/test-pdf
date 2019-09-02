0e643ed063b304e6c12c7a4d8456425bd5568da7
andkleven <amkleven@gmail.com>
2019-09-02 11:58:47 +0000

    Add existing file

---
+++ pdf.py
@@ -0,0 +1,4 @@
+import os
+variabel = 'file4.pdf'
+
+os.system("google-chrome --headless --disable-gpu --print-to-pdf="+str(variabel)+" http://li2055-247.members.linode.com/final_inspection/pipe/30/")
